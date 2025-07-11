b10b2fad31b96a43a8d8413f7dbe88092d4e2edb
Ayush Sahu <ayushsahu0716@outlook.com>
2025-07-11 21:47:31 +0100

    Add live score update and disable buttons after game ends

--- rock-paper-scissors/javascript.js
+++ rock-paper-scissors/javascript.js
@@ -2,66 +2,104 @@ function getComputerChoice() {
     let num = Math.random();
     let choice;
     if (num <= 1/3) {
-       choice = "Rock";
+       choice = "rock";
     }
     else if (num > 1/3 && num <= 2/3) {
-       choice = "Paper";
+       choice = "paper";
     }
     else {
-       choice = "Scissors";
+       choice = "scissors";
     }
 
     return choice;
 }
 
 
-
-
-function getHumanChoice() {
-    const prompt = require('prompt-sync')();
-    const choice = prompt("rock, paper or scissors")
-    return choice
-}
-
 humanScore = 0;
 computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-        if (computerChoice === "Scissors" && humanChoice === "rock") {
+const result = document.querySelector("#result");
+const content = document.createElement("div");
+content.classList.add("content");
+result.appendChild(content);
+
+const score = document.querySelector("#score");
+const scoreContent = document.createElement("div");
+content.classList.add("scoreContent");
+score.appendChild(scoreContent);
+
+const winner = document.querySelector("#winner");
+const winnerContent = document.createElement("div");
+winnerContent.classList.add("winnerContent");
+winner.appendChild(winnerContent);
+
+function playRound(humanChoice) {
+
+    if (humanScore === 5 || computerScore === 5) {
+        document.getElementById("rock").disabled = true;
+        document.getElementById("scissors").disabled = true;
+        document.getElementById("paper").disabled = true;
+        winnerContent.textContent = (humanScore === 5) ? "You win the game" : "You lost the game";
+        return;
+    }
+    const computerChoice = getComputerChoice();
+    let message = "";
+    
+        if (computerChoice === "scissors" && humanChoice === "rock") {
             humanScore += 1;
-            console.log("You Win")
+            message = "You win!";
         }
-        else if (computerChoice === "Rock" && humanChoice === "paper") {
+        else if (computerChoice === "rock" && humanChoice === "paper") {
              humanScore += 1;
-             console.log("You Win")
+             message = "You win!";
         }
-        else if (computerChoice === "Paper" && humanChoice === "scissors") {
+        else if (computerChoice === "paper" && humanChoice === "scissors") {
              humanScore += 1;
-             console.log("You Win")
+             message = "You win!";
 
         }
-        else if (computerChoice === "Scissors" && humanChoice === "paper") {
+        else if (computerChoice === "scissors" && humanChoice === "paper") {
              computerScore += 1;
-             console.log("You Lose")
+             message = "You lose!";
 
         }
-        else if (computerChoice === "Paper" && humanChoice === "rock") {
+        else if (computerChoice === "paper" && humanChoice === "rock") {
              computerScore += 1;
-             console.log("You Lose")
+            message = "You lose!";
 
         }
 
-        else if (computerChoice === "Rock" && humanChoice === "scissors") {
+        else if (computerChoice === "rock" && humanChoice === "scissors") {
             computerScore += 1;
-             console.log("You Lose")
+             message = "You lose!";
+
+        }
+
+        else if (computerChoice === "rock" && humanChoice === "rock") {
+             message = "Tie!";
+
+        }
+
+        else if (computerChoice === "scissors" && humanChoice === "scissors") {
+             message = "Tie!";
 
         }
 
 
+        else if (computerChoice === "paper" && humanChoice === "paper") {
+             message = "Tie!";
+
+        }
+
+
+        content.textContent = message;
+
+    scoreContent.textContent = `Score - You: ${humanScore} | Computer: ${computerScore}`;
     
 }
-for (let i = 0; i < 5; i++) { 
-    playRound(getHumanChoice(),getComputerChoice())
-}
 
-console.log(humanScore, computerScore)
+document.getElementById("rock").addEventListener("click", () => playRound("rock"));
+document.getElementById("scissors").addEventListener("click", () => playRound("scissors"));
+document.getElementById("paper").addEventListener("click", () => playRound("paper"));
+
+
